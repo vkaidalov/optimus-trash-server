71c10db533ab8e78aeb4016cbb9bc1de686e878a
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-05-10 00:04:10 +0300

    created `UserSerializer` with custom `create` and `update` methods

---
+++ users/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'email',
+                  'first_name', 'last_name', 'is_confirmed',)
+        write_only_fields = ('password',)
+        read_only_fields = ('is_confirmed', 'date_joined')
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+        )
+
+        user.set_password(validated_data['password'])
+
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+
+        instance.save()
+        return instance
---
+++ users/tests.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserSerializerTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            username='vlad', password='hello_world', first_name='johnny')
+
+    def test_with_full_data(self):
+        data = {
+            'username': 'sam',
+            'password': 'twinkle_little_star',
+            'email': 'sam@example.com',
+            'first_name': 'Sam',
+            'last_name': 'White'
+        }
+        serializer = UserSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+
+    def test_without_optional_data(self):
+        data = {
+            'username': 'john',
+            'password': 'john_ce)nation',
+        }
+        serializer = UserSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+
+    def test_without_required_fields(self):
+        data = {
+        }
+        serializer = UserSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+    def test_update_method(self):
+        user = User.objects.get_by_natural_key('vlad')
+        data = {
+            'username': 'endpoint',
+            'password': 'good',
+            'first_name': 'nick',
+            'last_name': 'bravo'
+        }
+        serializer = UserSerializer(user, data=data)
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+
+    def test_update_method_with_bad_email(self):
+        user = User.objects.get_by_natural_key('vlad')
+        data = {
+            'email': 'really bad email',
+        }
+        serializer = UserSerializer(user, data=data, partial=True)
+        self.assertFalse(serializer.is_valid())
