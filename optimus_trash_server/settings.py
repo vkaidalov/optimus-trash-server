f023ece44eaed28768b2da92e002a800516dcf90
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-05-10 23:15:52 +0300

    installed 'django-filter==2.1.0' and added to `INSTALLED_APPS`

--- optimus_trash_server/settings.py
+++ optimus_trash_server/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'users.apps.UsersConfig',
 ]
@@ -133,8 +134,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
 }
 
 
