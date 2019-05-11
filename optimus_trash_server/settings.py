71aedb7ed83e6be215c2feca41eefa5ec8223704
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-05-11 23:16:22 +0300

    installed 'djangorestframework-camel-case==1.0.3' and enabled its Parsers and a Renderer

--- optimus_trash_server/settings.py
+++ optimus_trash_server/settings.py
@@ -137,6 +137,17 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
 }
 
 
